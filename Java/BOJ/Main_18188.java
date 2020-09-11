37c3b9541d7602223db4d7bb61c518626a106edb
ohmozi <gown10195@gmail.com>
2020-09-12 00:19:21 +0900

    18188:solved

--- Java/BOJ/Main_18188.java
+++ Java/BOJ/Main_18188.java
@@ -14,11 +14,10 @@ public class Main_18188 {
     //
     //  3. BFS, 브루트포스
     static char mmap[][];
-    static boolean visited[][];
     static char Marid[][];
     static int dx[] = {0,-1,0,1};       // W,A,S,D
     static int dy[] = {-1,0,1,0};
-    static int H, W;
+    static int H, W, N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,50 +25,45 @@ public class Main_18188 {
         H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
         mmap = new char[H][W];
-        visited = new boolean[H][W];
         int D_X=0, D_Y=0, Z_X=0, Z_Y=0;
         for (int i = 0; i < H; i++) {
             String str = br.readLine();
             for (int j = 0; j < W; j++) {
                 mmap[i][j] = str.charAt(j);
-                if(mmap[i][j]=='@'){
-                    visited[i][j] = true;
-                } else if(mmap[i][j]=='D'){
+                if(mmap[i][j]=='D'){
                     D_Y = i;
                     D_X = j;
-                } else if(mmap[i][j]=='Z'){
-                    Z_Y = i;
-                    Z_X = j;
                 }
             }
         }
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         Marid = new char[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             Marid[i][0] = st.nextToken().charAt(0);
             Marid[i][1] = st.nextToken().charAt(0);
         }
-        BFS(D_X, D_Y, 0, Z_X, Z_Y, "");
+        BFS(D_X, D_Y, 0, "");
     }
-    public static void BFS(int i, int j, int c, int a, int b, String footPrint){
+    public static void BFS(int i, int j, int c, String footPrint){
         int next_x = 0;
         int next_y = 0;
         boolean isAnswer = false;
-        visited[j][i] = true;
         Queue<Loc> queue = new LinkedList<>();
         queue.add(new Loc(i, j, c, footPrint));
 
         while(!queue.isEmpty()){
             Loc l = queue.poll();
-            if(l.x == a && l.y == b){
+            if(mmap[l.y][l.x]=='Z'){
                 isAnswer = true;
                 System.out.println("YES");
-//                System.out.println(l.count);    // 일단 몇번 지나서 도착했는지 출력
                 System.out.println(l.footPrint);  // 경로저장해서 반환하기
                 break;
             }
-            boolean finish = true;
+            if(l.count==N)
+                continue;  // break가 아닌 continue
+//            D.....  -> DxxxX.   라지X에 있을때 오른쪽갔다가 N이라고 break하면 goal 못찾아가므로 continue
+//            ....Z.     ....Z.
             for (int k = 0; k < 2; k++) {
                 if(Marid[l.count][k]=='W'){
                     next_x = l.x + dx[0];
@@ -84,15 +78,10 @@ public class Main_18188 {
                     next_x = l.x + dx[3];
                     next_y = l.y + dy[3];
                 }
-//                만약 마리드가 제시한 두 방향 중 어느 방향으로도 다오가 갈 수 없다면, 다오는 더 이상 움직일 수 없다.
-                if(next_x>=0 && next_x<W && next_y>=0 && next_y<H && !visited[next_y][next_x]) {
-//                    finish = false;
-                    visited[next_y][next_x] = true;
+                if(next_x>=0 && next_x<W && next_y>=0 && next_y<H && mmap[next_y][next_x]!='@') {
                     queue.add(new Loc(next_x, next_y, l.count + 1, l.footPrint + Marid[l.count][k]));
                 }
             }
-//            if(finish)
-//                break;
         }
         if(!isAnswer){
             System.out.println("NO");
